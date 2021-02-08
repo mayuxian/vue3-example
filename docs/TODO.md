64c9aacbb75d184695af7ad1ee32ddc4e5ca97a4
mayuxian <mayuxian@autohome.com.cn>
2021-02-08 17:50:20 +0800

    添加

---
+++ docs/TODO.md
@@ -0,0 +1,1 @@
+https://juejin.cn/post/6924912613750996999
---
+++ src/layout/Demo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+
+interface SceneType {
+  id: number
+  name: string
+}
+export default defineComponent({
+  setup() {
+    const temp = new Array<SceneType>()
+    let t1 = {
+      id: 1,
+    } as SceneType
+    temp.push(t1)
+
+    const state1 = reactive<{
+      sceneType: Array<SceneType>
+    }>({
+      sceneType: temp,
+    })
+
+    const state = reactive({
+      SceneType: temp as Array<SceneType>,
+    })
+    return { ...toRefs(state) }
+  },
+})
+</script>
+
+<style scoped>
+</style>
